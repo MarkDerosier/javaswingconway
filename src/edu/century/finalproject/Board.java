88f129eb9f44e91dc40ca81fe41133ee16b685e5
MarkDerosier <markaderosier@icloud.com>
2018-06-27 18:09:34 -0500

    Added initial abstract class for board, and gitignore file

---
+++ src/edu/century/finalproject/Board.java
@@ -0,0 +1,30 @@
+package edu.century.finalproject;
+
+public abstract class Board {
+  private int height;
+  private int width;
+  private int board[][];
+
+  public Board(int width, int height) {
+    this.width = width;
+    this.height = height;
+    this.board = new int[height][width];
+  }
+
+  public int getWidth() {
+    return this.width;
+  }
+
+  public int getHeight() {
+    return this.height;
+  }
+
+  public int[][] showBoard() {
+    return this.board.clone();
+  }
+
+  public abstract void rules();
+
+  public abstract void intialize();
+
+}
